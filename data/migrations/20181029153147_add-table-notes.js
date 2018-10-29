9a1187c321ba203150c30893629cf08dde4aef1a
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-29 15:36:54 -0500

    created table/database for notes

---
+++ data/migrations/20181029153147_add-table-notes.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('notes', notes => {
+      notes.increments();
+      notes
+      .string('title', 128)
+      .notNullable()
+      .unique();
+      notes
+      .string('text body')
+      .notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('notes');
+};
