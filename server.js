39d50edb0b33b6e168209f7d9f5c1b7170da8d25
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-29 15:25:40 -0500

    server set up and functional

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
-const configureRoutes = require('./config/routes');
+//const configureRoutes = require('./config/routes');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-configureRoutes(server);
+// configureRoutes(server);
 
 module.exports = {
   server,
