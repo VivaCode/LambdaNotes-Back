592d2f6b682f6d8451cbc2b381263aa1bfac7130
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-11-01 12:59:45 -0500

    updated knex file for heroku database.

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,13 @@
+require('dotenv').config();
+const localPg = {
+  host: 'localhost',
+  database: 'lambda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
+
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -8,5 +18,20 @@ module.exports = {
       tableName: 'dbmigrations',
     },
     seeds: { directory: './data/seeds' },
+  },
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
   },
 };
